0051d87fe1e88c7d2c6a10a2ea9914b8d59e52a4
Lauren Plenger <laurenb08@gmail.com>
2020-10-22 23:21:59 -0700

    updated variables and objects to make sure prompts worked correctly

--- Develop/script.js
+++ Develop/script.js
@@ -3,42 +3,52 @@ var generateBtn = document.querySelector("#generate");
 
 //object that stores the arrays for generating the random password//
   var passwordCharacters = {
-    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-    var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
-    var charactersNeeded: [],
+    upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    specialChar: ["!", "@", "#", "$", "%", "^", "&", "*"],
+    charactersNeeded: [],
   }
+
+  console.log(passwordCharacters);
+  
   //Prompt user for password requirements including number and type of characters needed//
 function generatePassword() {
-
     var passLength = prompt("How long should your password be? (must be between 8-100 characters)");
-    alert("Your password will be " + passLength + " characters long.");
+    // alert("Your password will be " + passLength + " characters long.");
     var passUpper = confirm("Should your password include upper case letters?");
     var passLower = confirm("Should your password include lower case letters?");
     var passNum = confirm("Should your password include numbers?");
-    var passSpecial = confirm("Should your password include special characters?")
+    var passSpecial = confirm("Should your password include special characters?");
+
+  if (passLength < 8 || passLength > 100) {
+    alert("Please enter a password length between 8 and 100");
+    return;
+  }
 
   if (passUpper === true) {
-    passwordCharacters.charactersNeeded = passwordCharacters.passUpper.concat(passwordCharacters.charactersNeeded);
+    passwordCharacters.charactersNeeded = passwordCharacters.upperCase.concat(passwordCharacters.charactersNeeded);
     console.log(passwordCharacters.charactersNeeded)
   }
 
   if (passLower === true) {
-    passwordCharacters.charactersNeeded = passwordCharacters.passLower.concat(passwordCharacters.charactersNeeded);
+    passwordCharacters.charactersNeeded = passwordCharacters.lowerCase.concat(passwordCharacters.charactersNeeded);
     console.log(passwordCharacters.charactersNeeded)
   }
 
   if (passNum === true) {
-    passwordCharacters.charactersNeeded = passwordCharacters.passNum.concat(passwordCharacters.charactersNeeded);
+    passwordCharacters.charactersNeeded = passwordCharacters.numbers.concat(passwordCharacters.charactersNeeded);
     console.log(passwordCharacters.charactersNeeded)
   }
 
   if (passSpecial === true) {
-    passwordCharacters.charactersNeeded = passwordCharacters.passSpecial.concat(passwordCharacters.charactersNeeded);
+    passwordCharacters.charactersNeeded = passwordCharacters.specialChar.concat(passwordCharacters.charactersNeeded);
     console.log(passwordCharacters.charactersNeeded)
   }
 
+  for (i = 0; i < passLength + 1; i++) {
+    return;
+  }
 }
 
 // Write password to the #password input
@@ -47,6 +57,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
